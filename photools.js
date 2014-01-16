0c258111f4f09a7eeb452998da206b3bc0b07205
Diéssica Gurskas <diessica.gurskas@gmail.com>
2014-01-16 04:43:25 -0200

    Avoid query the DOM multiple times

--- photools.js
+++ photools.js
@@ -4,23 +4,31 @@
 * Description: Discover original size and source of Facebook photos
 */
 
-document.originalForm.onsubmit = function originalSize() {
-    // Get photo URL
-    var imgUrl       = document.originalForm.imgUrl.value,
-        sliceP1      = imgUrl.lastIndexOf("/"),
-        sliceP2      = imgUrl.lastIndexOf("_"),
-        idUrl        = imgUrl.slice(sliceP1, sliceP2);
-    // Open photo in original size
-    open("https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-ash3" + idUrl + "_n.jpg");
-};
-
-document.sourceForm.onsubmit = function photoSource() {
-    // Get photo URL
-    var imgUrl     = document.sourceForm.imgUrl.value,
-    // Get photo ID
-        sliceP1    = imgUrl.indexOf("_") + 1,
-        sliceP2    = imgUrl.indexOf("_", sliceP1),
-        idUrl      = imgUrl.slice(sliceP1, sliceP2);
-    // Open photo source
-    open("https://facebook.com/photo.php?fbid=" + idUrl);
-};
+(function(){
+    // Get original photo form
+    var originalForm = document.originalForm;
+    
+    originalForm.onsubmit = function originalSize() {
+        // Get photo URL
+        var imgUrl       = originalForm.imgUrl.value,
+            sliceP1      = imgUrl.lastIndexOf("/"),
+            sliceP2      = imgUrl.lastIndexOf("_"),
+            idUrl        = imgUrl.slice(sliceP1, sliceP2);
+        // Open photo in original size
+        open("https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-ash3" + idUrl + "_n.jpg");
+    };
+    
+    // Get photo source form
+    var sourceForm = document.sourceForm;
+    
+    sourceForm.onsubmit = function photoSource() {
+        // Get photo URL
+        var imgUrl     = sourceForm.imgUrl.value,
+        // Get photo ID
+            sliceP1    = imgUrl.indexOf("_") + 1,
+            sliceP2    = imgUrl.indexOf("_", sliceP1),
+            idUrl      = imgUrl.slice(sliceP1, sliceP2);
+        // Open photo source
+        open("https://facebook.com/photo.php?fbid=" + idUrl);
+    };
+})();
